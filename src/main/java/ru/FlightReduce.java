5c66a9b18627d58afe18016842a347747a3fe8f4
Вероника Александрова <veronikaaleksandrova@MBP-Veronika.Dlink>
2020-12-26 03:30:58 +0500

    Scripted auto-commit on change (2020-12-26 03:30:58) by gitwatch.sh

--- src/main/java/ru/FlightReduce.java
+++ src/main/java/ru/FlightReduce.java
@@ -4,6 +4,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import javax.naming.Context;
+import javax.xml.soap.Text;
 import java.io.IOException;
 import java.util.Iterator;
 
@@ -15,6 +16,24 @@ public class FlightReduce extends Reducer<FlightWritableComparable, Text, Text, 
     @Override
     protected void reduce(FlightWritableComparable key, Iterator<Text> values, Context context)
             throws IOException, InterruptedException {
-
+        Iterator<Text> iter = values.iterator();
+        Text nameInfo = new Text(iter.next());
+        int count = TOCOUNT;
+        float max, min, average = ZERO;
+        while (iter.hasNext()) {
+            float currentValue = Float.parseFloat(iter.next().toString());
+            if (count == TOCOUNT)
+                min = currentValue;
+            if (currentValue < min)
+                min = currentValue;
+            else if (currentValue > max)
+                max = currentValue;
+            average += currentValue;
+            count++;
+        }
+        if (count != TOCOUNT) {
+            average /= count;
+            context.write(nameInfo, new Text("MINDELAY: " + min + "; MAXDELAY: " + max + "; AVERAGE: " + average + ";"));
+        }
     }
 }
