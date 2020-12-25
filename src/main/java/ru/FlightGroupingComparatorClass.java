6ed5610aa8466b522efa06f6283937f59b4b4bad
Вероника Александрова <veronikaaleksandrova@MBP-Veronika.Dlink>
2020-12-26 03:00:00 +0500

    Scripted auto-commit on change (2020-12-26 03:00:00) by gitwatch.sh

--- src/main/java/ru/FlightGroupingComparatorClass.java
+++ src/main/java/ru/FlightGroupingComparatorClass.java
@@ -1,4 +1,13 @@
 package ru;
 
-public class FlightGroupingComparatorClass {
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
+
+public class FlightGroupingComparatorClass extends WriteComparator {
+    
+    protected FlightGroupingComparatorClass {
+        super(FlightWritableComparable.class, true);
+    }
+
+
 }
