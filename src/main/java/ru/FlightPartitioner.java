bf6dc00d1aa9724830073df90faeff740a4f40c0
Вероника Александрова <veronikaaleksandrova@MBP-Veronika.Dlink>
2020-12-26 03:45:52 +0500

    Scripted auto-commit on change (2020-12-26 03:45:52) by gitwatch.sh

--- src/main/java/ru/FlightPartitioner.java
+++ src/main/java/ru/FlightPartitioner.java
@@ -4,11 +4,8 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Partitioner;
 
 public class FlightPartitioner extends Partitioner<FlightWritableComparable, Text> {
-
     @Override
     public int getPartition(FlightWritableComparable key, Text text, int i) {
         return key.getID() % i;
     }
 }
-
-
