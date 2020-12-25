6b0405d9401d0337f3dfd94228c8f36b2fa51b7c
Вероника Александрова <veronikaaleksandrova@MBP-Veronika.Dlink>
2020-12-26 03:47:03 +0500

    Scripted auto-commit on change (2020-12-26 03:47:03) by gitwatch.sh

--- src/main/java/ru/FlightReduce.java
+++ src/main/java/ru/FlightReduce.java
@@ -2,31 +2,33 @@ package ru;
 
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
-import javax.naming.Context;
-import javax.xml.soap.Text;
 import java.io.IOException;
 import java.util.Iterator;
 
-public class FlightReduce extends Reducer<FlightWritableComparable, Text, Text, Text>{
+public class FlightReduce extends Reducer<FlightWritableComparable, Text, Text, Text> {
 
     private static final int TOCOUNT = 0;
     private static final float ZERO = 0.0F;
 
     @Override
-    protected void reduce(FlightWritableComparable key, Iterator<Text> values, Context context)
+    protected void reduce(FlightWritableComparable key, Iterable<Text> values, Context context)
             throws IOException, InterruptedException {
         Iterator<Text> iter = values.iterator();
         Text nameInfo = new Text(iter.next());
         int count = TOCOUNT;
-        float max, min, average = ZERO;
+        float min = ZERO;
+        float max = ZERO;
+        float average = ZERO;
         while (iter.hasNext()) {
             float currentValue = Float.parseFloat(iter.next().toString());
-            if (count == TOCOUNT)
+            if (count == TOCOUNT) {
                 min = currentValue;
-            if (currentValue < min)
+            }
+            if (currentValue < min) {
                 min = currentValue;
-            else if (currentValue > max)
+            } else if (currentValue > max) {
                 max = currentValue;
+            }
             average += currentValue;
             count++;
         }
