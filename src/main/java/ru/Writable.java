374c86da5a427a1bf1bbd9fe332fdee30628a9cb
Вероника Александрова <veronikaaleksandrova@MBP-Veronika.Dlink>
2020-12-26 01:28:24 +0500

    Scripted auto-commit on change (2020-12-26 01:28:24) by gitwatch.sh

--- src/main/java/ru/Writable.java
+++ src/main/java/ru/Writable.java
@@ -7,12 +7,14 @@ import java.io.DataOutput;
 import java.io.IOException;
 
 public class FlightWritable implements Writable {
-    
+
     public FlightWritable(int destAirportID, float arrDelay, float cancelled, float airTime) {
         this.destAirportID = destAirportID;
         this.arrDelay = arrDelay;
         this.cancelled = cancelled;
         this.airTime = airTime;
     }
+
+    
 }
 
