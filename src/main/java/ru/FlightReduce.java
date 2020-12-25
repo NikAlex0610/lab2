137a88e18e86cf4f8b92d2140eafb0d7e9c01ec6
Вероника Александрова <veronikaaleksandrova@MBP-Veronika.Dlink>
2020-12-26 03:22:21 +0500

    Scripted auto-commit on change (2020-12-26 03:22:21) by gitwatch.sh

--- src/main/java/ru/FlightReduce.java
+++ src/main/java/ru/FlightReduce.java
@@ -2,10 +2,19 @@ package ru;
 
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
+
+import javax.naming.Context;
 import java.io.IOException;
 import java.util.Iterator;
 
 public class FlightReduce extends Reducer<FlightWritableComparable, Text, Text, Text>{
 
-    
+    private static final int TOCOUNT = 0;
+    private static final float ZERO = 0.0F;
+
+    @Override
+    protected void reduce(FlightWritableComparable key, Iterator<Text> values, Context context)
+            throws IOException, InterruptedException {
+
+    }
 }
