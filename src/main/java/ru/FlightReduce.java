5b7092de0f4ba5c559b7b00317e6fcaae743c81a
Вероника Александрова <veronikaaleksandrova@MBP-Veronika.Dlink>
2020-12-26 03:15:24 +0500

    Scripted auto-commit on change (2020-12-26 03:15:24) by gitwatch.sh

--- src/main/java/ru/FlightReduce.java
+++ src/main/java/ru/FlightReduce.java
@@ -1,2 +1,9 @@
-package ru;public class FlightReduce {
+package ru;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+import java.io.IOException;
+import java.util.Iterator;
+
+public class FlightReduce {
 }
