907cad30758fef89158dbcd27ca31cce386167d9
Вероника Александрова <veronikaaleksandrova@MBP-Veronika.Dlink>
2020-12-26 03:42:34 +0500

    Scripted auto-commit on change (2020-12-26 03:42:34) by gitwatch.sh

--- src/main/java/ru/FlightGroupingComparatorClass.java
+++ src/main/java/ru/FlightGroupingComparatorClass.java
@@ -1,16 +1,16 @@
-package ru;
+package ru.Ivan;
 
 import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
 
-public class FlightGroupingComparatorClass extends WriteComparator {
+public class FlightGroupingComparatorClass extends WritableComparator {
 
-    protected FlightGroupingComparatorClass {
+    protected FlightGroupingComparatorClass() {
         super(FlightWritableComparable.class, true);
     }
 
     @Override
-    public int compare(WritableComparable a, WritableComparable b) {
+    public int compare(WritableComparable a,WritableComparable b) {
         FlightWritableComparable firstFlight = (FlightWritableComparable) a;
         FlightWritableComparable secondFlight = (FlightWritableComparable) b;
         return firstFlight.compareID(secondFlight);
