28f28431f8a62f9f6ed5d15eb8544ade85955246
Вероника Александрова <veronikaaleksandrova@MBP-Veronika.Dlink>
2020-12-26 01:57:44 +0500

    Scripted auto-commit on change (2020-12-26 01:57:44) by gitwatch.sh

--- src/main/java/ru/FlightMapper.java
+++ src/main/java/ru/FlightMapper.java
@@ -1,4 +1,10 @@
 package ru;
 
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
 public class FlightMapper extends Mapper<LongWritable, Text, FlightWritableComparable, Text> {
 }
